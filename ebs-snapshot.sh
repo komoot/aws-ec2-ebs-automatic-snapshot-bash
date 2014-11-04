c96ae7233aa0e008173032017ef95fbcb4444189
jstaffans <johannes.staffans@gmail.com>
2014-11-04 11:00:51 +0100

    Use region for all aws commands

--- ebs-snapshot.sh
+++ ebs-snapshot.sh
@@ -121,32 +121,32 @@ do
 	echo "Volume ID is $volume_id" >> $logfile
     
 	# Next, we're going to take a snapshot of the current volume, and capture the resulting snapshot ID
-	snapresult=$(aws ec2 create-snapshot --output=text --description $description --volume-id $volume_id --query SnapshotId)
+	snapresult=$(aws ec2 create-snapshot --region $region --output=text --description $description --volume-id $volume_id --query SnapshotId)
 	
     echo "New snapshot is $snapresult" >> $logfile
          
     # And then we're going to add a "CreatedBy:AutomatedBackup" tag to the resulting snapshot.
     # Why? Because we only want to purge snapshots taken by the script later, and not delete snapshots manually taken.
-    aws ec2 create-tags --resource $snapresult --tags Key=CreatedBy,Value=AutomatedBackup
+    aws ec2 create-tags --region $region --resource $snapresult --tags Key=CreatedBy,Value=AutomatedBackup
 done
 
 # Get all snapshot IDs associated with each volume attached to this instance
 rm /tmp/snapshot_info.txt --force
 for vol_id in $(cat /tmp/volume_info.txt)
 do
-    aws ec2 describe-snapshots --output=text --filters "Name=volume-id,Values=$vol_id" "Name=tag:CreatedBy,Values=AutomatedBackup" --query Snapshots[].SnapshotId | tr '\t' '\n' | sort | uniq >> /tmp/snapshot_info.txt 2>&1
+    aws ec2 describe-snapshots --region $region --output=text --filters "Name=volume-id,Values=$vol_id" "Name=tag:CreatedBy,Values=AutomatedBackup" --query Snapshots[].SnapshotId | tr '\t' '\n' | sort | uniq >> /tmp/snapshot_info.txt 2>&1
 done
 
 # Purge all instance volume snapshots created by this script that are older than 7 days
 for snapshot_id in $(cat /tmp/snapshot_info.txt)
 do
     echo "Checking $snapshot_id..."
-	snapshot_date=$(aws ec2 describe-snapshots --output=text --snapshot-ids $snapshot_id --query Snapshots[].StartTime | awk -F "T" '{printf "%s\n", $1}')
+	snapshot_date=$(aws ec2 describe-snapshots --region $region --output=text --snapshot-ids $snapshot_id --query Snapshots[].StartTime | awk -F "T" '{printf "%s\n", $1}')
     snapshot_date_in_seconds=`date "--date=$snapshot_date" +%s`
 
     if (( $snapshot_date_in_seconds <= $retention_date_in_seconds )); then
         echo "Deleting snapshot $snapshot_id ..." >> $logfile
-        aws ec2 delete-snapshot --snapshot-id $snapshot_id
+        aws ec2 delete-snapshot --region $region --snapshot-id $snapshot_id
     else
         echo "Not deleting snapshot $snapshot_id ..." >> $logfile
     fi
